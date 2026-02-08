36cc68314739a07439e22fa0213e9d005798803d
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-02-08 12:38:04 +0600

    Update lab_1.py

--- Sec_B_Lab_1/lab_1.py
+++ Sec_B_Lab_1/lab_1.py
@@ -1,1 +1,32 @@
-print('Hello')
+import turtle
+import random
+import time
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title('Turtle is fun!')
+
+t = turtle.Turtle()
+t.speed(0)
+t.pensize(2)
+
+palette = ['cyan', 'magenta', 'yellow', 'lime', 'orange', 'white', 'red', 'blue', 'green']
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+
+for i in range(200):
+    t.pencolor(random.choice(palette))
+    t.forward(i * 2)
+    t.right(91)
+
+t.penup()
+t.goto(-180, -250)
+t.pencolor("white")
+t.write("Python is fun!", font=("Arial", 16, "normal"))
+
+t.hideturtle()
+time.sleep(5)
+
+turtle.done()
